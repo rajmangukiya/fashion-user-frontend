717edbc61b4059c8115ca1d740b292afe5d99984
Jaydeep Ghevariya <72187052+JYDP02@users.noreply.github.com>
2023-06-08 11:23:37 +0530

    formal check

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,9 @@
+import Banner from "./Banner/Banner";
 import MaxCarousel from "./MaxCarousel/MaxCarousel";
 import MinCarousel from "./MinCarousel/MinCarousel";
 
 export {
+    Banner,
     MaxCarousel,
     MinCarousel
 }
