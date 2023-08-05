6cfcc1dbba459ef338a3ceb06294a8259aa9bf36
rajmangukiya <rajmangukiya00@gmail.com>
2023-08-05 12:36:42 +0530

    explore removed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <>
         <div className="d-flex justify-content-center align-items-center">
             <img style={{objectFit: 'cover'}} className='banner w-100' src='https://ampm.in/cdn/shop/files/Image-1.jpg?v=1686898796' />
-            <a style={{fontFamily: 'Ysabeau Office, sans-serif'}} className='position-absolute text-dark bg-white px-5 py-2 fs-4' href='#category-carousel' >Explore</a>
+            {/* <a style={{fontFamily: 'Ysabeau Office, sans-serif'}} className='position-absolute text-dark bg-white px-5 py-2 fs-4' href='#category-carousel' >Explore</a> */}
             {/* <div className="banner-overlay">
             </div> */}
         </div>
